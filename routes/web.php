2c456d967f43dff718a49dcb11756561fbbca446
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-06-09 03:34:21 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\ProdutoController;
 Route::get('/', function () {
     return view('welcome');
 });
+//Set Rotas parao CRUD de Computadors
+Route::resource('/produtos', App\Http\Controllers\ProdutoController::class);
+Route::resource('/produto', App\Http\Controllers\ProdutoController::class);
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::get('/produtos/{id}', [ProdutoController::class, 'show']);
