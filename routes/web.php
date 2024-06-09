c4907c03ed4d026f97027c7a115e1f2b9b92de94
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-06-09 02:57:21 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::get('/produtos/{id}', [ProdutoController::class, 'show']);
+
